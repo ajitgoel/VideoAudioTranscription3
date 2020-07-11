fa4cd96f58265787cf59058b35d0a4045c6e4fa0
Ajit Goel <ajitgoel@gmail.com>
2020-07-10 19:07:38 -0500

    transcribeaudiovideo function=>draft

--- amplify/backend/function/transcribeaudiovideo/src/transcribeaudiovideo.cs
+++ amplify/backend/function/transcribeaudiovideo/src/transcribeaudiovideo.cs
@@ -3,9 +3,10 @@ using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Newtonsoft.Json;
-
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+using Amazon.TranscribeService;
+using Amazon.TranscribeService.Model;
+using Amazon;
+using System;
 
 // If you rename this namespace, you will need to update the invocation shim
 // to match if you intend to test the function with 'amplify mock function'
@@ -14,21 +15,54 @@ namespace transcribeaudiovideo
     // If you rename this class, you will need to update the invocation shim
     // to match if you intend to test the function with 'amplify mock function'
     public class transcribeaudiovideo
-    {
-        /// <summary>
-        /// A Lambda function to respond to HTTP Get methods from API Gateway
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns>The list of blogs</returns>
-        /// <remarks>
-        /// If you rename this function, you will need to update the invocation shim
-        /// to match if you intend to test the function with 'amplify mock function'
-        /// </remarks>
+    {        
+        private string _sourceBucket = "";
+        private string _writeBucket = "";
+        private string _prefix = "";
+        private IAmazonTranscribeService iAmazonTranscribeService;
+
         #pragma warning disable CS1998
-        public async Task LambdaHandler(S3Event s3Event, ILambdaContext context)
+        public async Task LambdaHandler(S3Event s3Event, ILambdaContext iLambdaContext)
         {
-            context.Logger.LogLine($"s3Event: {JsonConvert.SerializeObject(s3Event)}\n");
+          iAmazonTranscribeService = new AmazonTranscribeServiceClient();
+          var objectKey = s3Event.Records[0].S3.Object.Key;
+          if (objectKey == null)
+          {
             return;
+          }
+
+          try
+          {
+            var transcribeJobName = "Job-";
+            var fileLocation = "https://" + _sourceBucket + _prefix + objectKey;
+            var startTranscriptionJobRequest = new StartTranscriptionJobRequest()
+            {
+              TranscriptionJobName = transcribeJobName,
+              MediaFormat = MediaFormat.Wav.ToString(),
+              Media ={MediaFileUri = fileLocation},
+              LanguageCode = LanguageCode.EnGB.ToString(),
+              OutputBucketName = _writeBucket
+            };
+            var startTranscriptionJobResponse = await iAmazonTranscribeService.StartTranscriptionJobAsync(startTranscriptionJobRequest);
+            var upper = 1;
+            for (var i = 0; i < upper; i++)
+            {
+              if (startTranscriptionJobResponse.TranscriptionJob.TranscriptionJobStatus.ToString() != "Completed" ||
+                  startTranscriptionJobResponse.TranscriptionJob.TranscriptionJobStatus.ToString() != "Failed")
+              {
+
+              }
+              else
+              {
+                iLambdaContext.Logger.LogLine($"StartTranscriptionJobResponse : {JsonConvert.SerializeObject(startTranscriptionJobResponse)}\n");
+              }
+            }
+          }
+          catch (Exception exception)
+          {
+            iLambdaContext.Logger.LogLine($"Exception : {JsonConvert.SerializeObject(exception)}\n");
+            throw;
+          }
         }
     }
 }
